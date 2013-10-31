742ae2a76032de0b7b34ee721a4690fec0aa6371
Gabriele Grecchi <gabriele.grecchi@gmail.com>
2013-10-31 14:53:43 +0100

    Add image upload with Paperclip

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -53,6 +53,6 @@ class TransactionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def transaction_params
-      params.require(:transaction).permit(:category, :date, :amount, :location, :account, :tag)
+      params.require(:transaction).permit(:category, :date, :amount, :location, :account, :tag, :image)
     end
 end
