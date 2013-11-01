0a5224e04ce3aff8aad9b5116fc74b1c9a902f4d
Gabriele Grecchi <gabriele.grecchi@gmail.com>
2013-11-01 11:51:48 +0100

    Add names to users 2

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,7 @@ class Transaction < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
+	validates :category, presence: true
+	validates :amount, presence: true
 end
